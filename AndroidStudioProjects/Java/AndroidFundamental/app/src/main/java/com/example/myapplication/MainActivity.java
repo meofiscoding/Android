14807ef3f020c3ef249583f35c885309703df351
Thanh tra <tranthe150186@fpt.edu.vn>
2022-05-23 23:11:39 +0700

    commit adding Log statement

--- AndroidStudioProjects/Java/AndroidFundamental/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidStudioProjects/Java/AndroidFundamental/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Add log statement
+        Log.d("MainActivity", "Hello World");
     }
 }
