83e23d51b664d872ce7fcce9b3d0b2f7fb6aeb4f
Thanh tra <tranthe150186@fpt.edu.vn>
2022-05-28 22:54:51 +0700

    commit HelloToast Applicaiton

--- AndroidStudioProjects/Java/AndroidFundamental/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidStudioProjects/Java/AndroidFundamental/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,38 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    //    get references to other UI elements in the layout,
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
         //Add log statement
-        Log.d("MainActivity", "Hello World");
+//        Log.d("MainActivity", "Hello World");
+    }
+
+    //    the Toast Button click handler
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+//        The duration of a Toast display can be either Toast.LENGTH_LONG or Toast.LENGTH_SHORT. The actual lengths are about 3.5 seconds for the long Toast and 2 seconds for the short Toast.
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
