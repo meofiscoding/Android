b5ffc3b549522280d95d579dffbca9f0ebfa05b3
meof-coding <tranthe150186@fpt.edu.vn>
2022-07-01 11:02:30 +0700

    commit biometric authentication

--- AndroidStudioProjects/Java/Projects/UpTodo/app/src/main/java/com/example/myapplication/StartActivity.java
+++ AndroidStudioProjects/Java/Projects/UpTodo/app/src/main/java/com/example/myapplication/StartActivity.java
@@ -1,21 +1,31 @@
 package com.example.myapplication;
 
+import static androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG;
+import static androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.example.myapplication.ui.login.LoginActivity;
+
+import java.util.concurrent.Executor;
 
 public class StartActivity extends AppCompatActivity {
     ImageView back;
     Button login;
     Button register;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
