d3eb74c7b58c2b36ef8435fa2490a68bfcfbbbeb
Thanh tra <tranthe150186@fpt.edu.vn>
2022-07-03 17:54:09 +0700

    commit fragment home

--- AndroidStudioProjects/Java/Projects/UpTodo/app/src/main/java/com/example/myapplication/ui/index/HomeActivity.java
+++ AndroidStudioProjects/Java/Projects/UpTodo/app/src/main/java/com/example/myapplication/ui/index/HomeActivity.java
@@ -2,15 +2,36 @@ package com.example.myapplication.ui.index;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.myapplication.R;
+import com.example.myapplication.SplashScreen;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
+    //Init variable Firebase
+    FirebaseAuth mAuth;
+    //Binding
+//    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mAuth = FirebaseAuth.getInstance();
+
+//        binding
+    }
+
+    //Check if user is authenticated or not
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        if (mUser == null) {
+            startActivity(new Intent(HomeActivity.this, SplashScreen.class));
+        }
     }
 }
