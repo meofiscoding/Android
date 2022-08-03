73833b043ff1445c718119ae30d7ead3774c3cab
Thanh tra <tranthe150186@fpt.edu.vn>
2022-08-03 23:12:24 +0700

    commit change

--- AndroidStudioProjects/Java/Projects/UpTodo/app/src/main/java/com/example/myapplication/adapter/TaskAdapter.java
+++ AndroidStudioProjects/Java/Projects/UpTodo/app/src/main/java/com/example/myapplication/adapter/TaskAdapter.java
@@ -1,25 +1,65 @@
 package com.example.myapplication.adapter;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication.R;
+import com.example.myapplication.model.Task;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.chip.Chip;
 
-public class TaskAdapter extends FirebaseRecyclerAdapter {
-    class myViewHolder extends RecyclerView.ViewHolder {
-        
+public class TaskAdapter extends FirebaseRecyclerAdapter<Task, TaskAdapter.myViewHolder> {
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public TaskAdapter(@NonNull FirebaseRecyclerOptions<Task> options) {
+        super(options);
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position, @NonNull Object model) {
-
+    protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull Task model) {
+        holder.taskTittle.setText(model.getTask());
+        holder.categoryName.setText(model.getCategory().categoryName);
+        //humanizer dateTime
+        //holder.timastamp
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.newtask_row,parent,false);
+        return new myViewHolder(view);
     }
+
+    class myViewHolder extends RecyclerView.ViewHolder {
+        RadioButton checkButton;
+        TextView taskTittle;
+        TextView timeStamp;
+        ImageView priority_flag;
+        Chip categoryName;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            checkButton = (RadioButton) itemView.findViewById(R.id.check_button);
+            taskTittle = (TextView) itemView.findViewById(R.id.taskName);
+            timeStamp = (TextView) itemView.findViewById(R.id.endDate);
+            priority_flag = (ImageView) itemView.findViewById(R.id.priority_flag);
+            categoryName = (Chip) itemView.findViewById(R.id.categoryName);
+        }
+
+    }
+
 }
