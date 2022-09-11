01099c32637ffa203db823febc3ee4b6f4984d76
meof-coding <tranthe150186@fpt.edu.vn>
2022-09-11 11:37:10 +0700

    test commit

--- AndroidStudioProjects/Java/Projects/UpTodo/app/src/main/java/com/example/myapplication/ui/index/HomeActivity.java
+++ AndroidStudioProjects/Java/Projects/UpTodo/app/src/main/java/com/example/myapplication/ui/index/HomeActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.MotionEvent;
-import android.view.View;
+import android.view.View;  
 import android.view.inputmethod.InputMethodManager;
 import android.widget.CalendarView;
 import android.widget.ImageView;
