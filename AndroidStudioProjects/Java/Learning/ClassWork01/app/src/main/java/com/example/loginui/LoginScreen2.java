7ceb2f27c853117f604b3fcf72f5787673edf5f9
meof-coding <tranthe150186@fpt.edu.vn>
2022-10-05 13:13:34 +0700

    [Share Preference] commit change

--- AndroidStudioProjects/Java/Learning/ClassWork01/app/src/main/java/com/example/loginui/LoginScreen2.java
+++ AndroidStudioProjects/Java/Learning/ClassWork01/app/src/main/java/com/example/loginui/LoginScreen2.java
@@ -3,10 +3,13 @@ package com.example.loginui;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,26 +17,45 @@ public class LoginScreen2 extends AppCompatActivity {
     private Button btnLogin;
     private EditText edtPassword;
     private EditText edtUsername;
+    CheckBox cb_check;
+    SharedPreferences spf;
+    SharedPreferences.Editor editor;
+    private static String USERNAME_KEY = "username";
 
     private void bindingView(){
         btnLogin = findViewById(R.id.btn_login);
         edtPassword = findViewById(R.id.edtpassword);
         edtUsername = findViewById(R.id.edtusername);
+        cb_check = findViewById(R.id.cb_remember);
     }
 
     private void onLoginClick(View view){
-//        if (edtPassword.getText().toString().equals("123456") && edtUsername.getText().toString().equals("admin")){
-//            Toast.makeText(LoginScreen2.this, "Login successfully!!", Toast.LENGTH_SHORT).show();
-//            //this present for current context
-//            Intent i = new Intent(this, HomeActivity.class);
+        String userName = edtUsername.getText().toString();
+        String password = edtPassword.getText().toString();
+        if (cb_check.isChecked()) {
+            editor = spf.edit();
+            editor.putString(USERNAME_KEY, userName);
+            editor.putString("password", password);
+            editor.putBoolean("checked", cb_check.isChecked());
+            editor.commit();
+        } else {
+            editor = spf.edit();
+            editor.remove(USERNAME_KEY);
+            editor.remove("password");
+            editor.remove("checked");
+            editor.commit();
+        }
+        if (password.equals("123456") && userName.equals("admin")){
+            Toast.makeText(LoginScreen2.this, "Login successfully!!", Toast.LENGTH_SHORT).show();
+            //this present for current context
+            Intent i = new Intent(this, HomeActivity.class);
 //            i.putExtra("name",edtUsername.getText().toString());
 //            startActivityForResult(i,1);
-////            startActivity(i);
-//        }else{
-//            Toast.makeText(LoginScreen2.this, "Login failed", Toast.LENGTH_SHORT).show();
-//        }
-                    Intent i = new Intent(this, HomeActivity.class);
-                    startActivity(i);
+            startActivity(i);
+        }else{
+            Toast.makeText(LoginScreen2.this, "Login failed", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     public void bindingAction(){
@@ -48,6 +70,13 @@ public class LoginScreen2 extends AppCompatActivity {
         setContentView(R.layout.activity_login_screen2);
         bindingView();
         bindingAction();
+        spf = this.getSharedPreferences("account_storage", Context.MODE_PRIVATE);
+        String usn_str = spf.getString(USERNAME_KEY, "");
+        String pass_str = spf.getString("password", "");
+        boolean check_stt = spf.getBoolean("checked", false);
+        edtUsername.setText(usn_str);
+        edtPassword.setText(pass_str);
+        cb_check.setChecked(check_stt);
     }
 
     @Override
