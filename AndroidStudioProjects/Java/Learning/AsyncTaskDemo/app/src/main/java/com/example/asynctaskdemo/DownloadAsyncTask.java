e4f265c251973150be761ae3f8f5cd8ebec811f8
dev17sinka <82217333+meof-coding@users.noreply.github.com>
2022-10-12 11:28:57 +0700

    commit 20%

---
+++ AndroidStudioProjects/Java/Learning/AsyncTaskDemo/app/src/main/java/com/example/asynctaskdemo/DownloadAsyncTask.java
@@ -0,0 +1,7 @@
+package com.example.asynctaskdemo;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+public class DownloadAsyncTask extends AsyncTask<String, String, Bitmap> {
+}
--- AndroidStudioProjects/Java/Learning/AsyncTaskDemo/app/src/main/java/com/example/asynctaskdemo/MainActivity.java
+++ AndroidStudioProjects/Java/Learning/AsyncTaskDemo/app/src/main/java/com/example/asynctaskdemo/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.asynctaskdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ProgressBar progressbar;
+    private ImageView imv;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindingViews();
+        bindingAction();
+        imv = findViewById(R.id.imageView);
+        progressbar = findViewById(R.id.progressBar);
+        String url = "https://baoquocte.vn/stores/news_dataimages/linhnguyen/112020/21/08/4940_hoa-hau-viet-nam-2020-1-1605897132611.jpg?rt=20201121084941";
+    }
+
+    private void bindingViews() {
+        imv = findViewById(R.id.imageView);
+        progressbar = findViewById(R.id.progressBar);
+        button = findViewById(R.id.btnDownload);
+    }
+
+    private void bindingAction() {
+        button.setOnClickListener(this::onDownloadImg);
+    }
+
+    private void onDownloadImg(View view) {
+        DownloadAsyncTask asyn = new DownloadAsyncTask();
+        asyn.execute(url);
     }
 }
